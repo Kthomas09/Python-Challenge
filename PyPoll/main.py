b8dae0ca5697447aa7c749408c244df51ef481c7
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-07-16 21:23:43 -0400

    Added total votes for loop, and set up election results table

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,5 +10,17 @@ poll_data_csv = os.path.join("PyPoll/Resources/election_data.csv")
 with open(poll_data_csv) as poll_data:
     reader =csv.reader(poll_data)
     header = next(poll_data)
-    print (header)
+    # print (header)
     
+    total_votes = 0
+    for row in poll_data:
+        total_votes += 1
+
+
+
+# Election Results Table
+print("Election Results")
+print("---------------------------")
+print(f"Total Votes: {total_votes}")
+print("---------------------------")
+
