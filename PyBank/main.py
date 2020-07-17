12ac8eda0f18157df90c3f45d11f604a70f5588b
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-07-17 01:43:58 -0400

    Tried alot of things out to simplfy code. Need to continue to debug.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,48 +8,59 @@ import csv
 #Budget Data Pathway
 budget_data_csv =  os.path.join("PyBank/Resources/budget_data.csv")
 
- # Variables for Total Months and Total Net Profit
-
+#Variables for Total Months and Total Net Profit
+total_months = 0
+total_profits = 0
+Greatest_Increase = 0
+Greatest_Decrease = 0
 
 #Opening 
 with open(budget_data_csv) as budget_data:
     Budget_Reader = csv.reader(budget_data)
     header = next(budget_data)
-    # print(header) 
-    Total_Months = 0
-    Total_Net_Profit = 0
-    Greatest_Increase = 0
-    Greatest_Decrease = 0
-    # For loop to iterate over Budget data to, 
-    # calculate Total Months and Total Profit.
-    for row in Budget_Reader: 
-        Total_Months+=1
-        Total_Net_Profit = Total_Net_Profit + int(row[1])
-
-    # # Variable/List for the difference between months in profits
-    #     Profit_Difference_List = []
-    #     Profit_Difference = 0
-    #     Average_Profit = 0
-
-    # # For loop to iterate through CSV and calculate the difference between profits.
-    # for row in Budget_Reader:
-    #     Profit_Difference = Budget_Reader(row[1]-1)- Budget_Reader(row[1])
-    #     Profit_Difference_list += [Profit_Difference]
     
-    # Sum_of_Difference = sum(Profit_Difference_List)
-    # Average_Profit = (Sum_of_Difference)/range(len(Profit_Difference_List)
+# not working
+    # dates = []
+    # for row1 in Budget_Reader:
+    #     dates.append(row1[0])
+    #     break
+    # print(dates)
 
-    # for row in Budget_Reader:
-    #     if Budget_Reader(row[1]+1) > Budget_Reader(row[1]):
-    #         Greatest_Increase = Budget_Reader(row[1]+1)
-    #     elif Budget_Reader(row[1]+1) < Budget_Reader(row[1]):
-    #         Greatest_Decrease = Budget_Reader(row[1]+1])
+    # profits = []
+    # for row2 in Budget_Reader:
+    #     profits.append(row2[1])
+    #     break
+    # print(profits)
 
-# Printing Financial Analysis Table
+    # For loop to iterate over Budget data to calculate Total Months and Total Profit.
+    for row in Budget_Reader: 
+        total_months+=1
+        total_profits = total_profits + int(row[1])
+
+    # Variable/List for the difference between months in profits
+    # profit_diff = 0
+    
+# For loop to iterate through CSV and calculate the difference between profits.
+    # for row in range(len(profits)-1):
+    #     profit_diff = int(profits[row+1]) - int(profits[row])
+    #     list_profit_diff.append(profit_diff)
+    
+    # sum_of_profit = sum(list_profit_diff)
+    # average_profit = sum_of_profit/len(profits)
+
+
+    # # for row in profit
+    # if Budget_Reader(row[1]+1) > Budget_Reader(row[1])
+    #     Greatest_Increase = (f"{Budget_Reader(row[1]+1)},{Budget_Reader(row[0]+1)}")
+    # elif Budget_Reader(row[1]+1) < Budget_Reader(row[1]):
+    #     Greatest_Decrease = Budget_Reader(row[1]+1])
+
+
+# # Printing Financial Analysis Table
 print("Financical Analysis")
 print("-----------------")
-print(f"Total Months: {Total_Months}")
-print("Total: " + str(Total_Net_Profit))
-# print("Average Profit Gain/Loss $" + str(Average_Profit))
-# print("Greatest % Increase: "  + str(Greatest_Increase))
-# print("Greatest % Decrease: " + str(Greatest_Decrease))
+print(f"Total Months: {total_months}")
+print("Total: " + str(total_profits))
+# # # print("Average Profit Gain/Loss $" + str(Average_Profit))
+# # # print("Greatest % Increase: "  + str(Greatest_Increase))
+# # # print("Greatest % Decrease: " + str(Greatest_Decrease))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,21 +10,24 @@ poll_data_csv = os.path.join("PyPoll/Resources/election_data.csv")
 with open(poll_data_csv) as poll_data:
     reader =csv.reader(poll_data)
     header = next(poll_data)
-    print (header)
-    
-    
-    
+    # print (header)
+
     #Variable for Total Votes
     total_votes = 0
+    
+    #Function to tabulate the votes
+    def poll_tabulation (poll_data):
+        candidate = str(poll_data[2])
     #For loop to iterate through polling data to calculate total votes and reassign total votes variable.
-    for row in poll_data:
-        total_votes += 1
+        for row in poll_data:
+            total_votes += 1
+            candidate = str(row[2])
+        print (str(candidate))
 
 
-
-# Election Results Table
-print("Election Results")
-print("---------------------------")
-print(f"Total Votes: {total_votes}")
-print("---------------------------")
+# # Election Results Table
+# print("Election Results")
+# print("---------------------------")
+# print(f"Total Votes: {total_votes}")
+# print("---------------------------")
 
