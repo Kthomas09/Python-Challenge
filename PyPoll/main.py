6f3f0c960bcebd50c444a389b7df156f6dc5785b
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-07-16 23:17:28 -0400

    Various changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,9 +10,13 @@ poll_data_csv = os.path.join("PyPoll/Resources/election_data.csv")
 with open(poll_data_csv) as poll_data:
     reader =csv.reader(poll_data)
     header = next(poll_data)
-    # print (header)
+    print (header)
     
+    
+    
+    #Variable for Total Votes
     total_votes = 0
+    #For loop to iterate through polling data to calculate total votes and reassign total votes variable.
     for row in poll_data:
         total_votes += 1
 
