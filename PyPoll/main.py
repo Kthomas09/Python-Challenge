dac0f3cf311309d2fa206ced1dd36265f535642d
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-07-19 23:50:34 -0400

    bug is on line 48

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,41 +22,35 @@ with open(filepath,newline="") as polldata:
     electionReader = csv.reader(polldata)
     # variable skips header
     header = next(polldata)
-    # print(header)
 
     #go through each row to tabulate votes
     for row in electionReader:
         votes = votes + 1
-        # print(votes)
         candidate = row[2]
-        # print(candidate)
 
         #tabulating candidate votes to candidateTable
         if candidate in candidateTable:
             candidateTotals = candidateTable.index(candidate)
             ballotCount[candidateTotals] = ballotCount[candidateTotals] + 1
-        #else append 1 vote to candidate and add a new row to lists
         else:
             candidateTable.append(candidate)
             ballotCount.append(1)
-        # print(candidateTable)
-    print(ballotCount)
-# #Variables for percentage of votes
-# percentages = []
-# maxVotes = ballotCount[0]
-# maxName = 0
-
-# #Find the percentage of votes for each candidate
-# for count in range(len(candidateTable)):
-#     votePercentage = ballotCount[count]/votes*100
-#     percentages.append(votePercentage)
-#     # printTable = f"{candidateTable[count]}: {percentages[count]:.3f}% ({ballotCount[count]})"
-#     # Determines winner
+# ==============================================================================
+#Variables for percentage of votes
+percentages = []
+maxVotes = ballotCount[0]
+maxName = 0
+#Find the percentage of votes for each candidate
+for count in range(len(candidateTable)):
+    votePercentage = ballotCount[count]/votes*100
+    percentages.append(votePercentage)
+# print(percentages)
+print(f"{candidateTable[count]}: {percentages[count]:.3f}% ({ballotCount[count]})")
+    # Determines winner
 #     if ballotCount[count] > maxVotes:
 #         maxVotes = ballotCount[count]
 #         maxName = count
-
-# # Winner Variable
+# # # Winner Variable
 # winner = candidateTable[maxName]
 # print(f"Election Results")
 # print(f"---------------------")
